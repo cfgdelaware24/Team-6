61323e1549be423b7735cedb91eab41686a19acd
arjavjain13 <jainarjavs@gmail.com>
2024-10-05 07:30:38 -0400

    adding new route, controller, and model for getting all user data

--- backend/Routes/authRoutes.js
+++ backend/Routes/authRoutes.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-const { getRiskAssessmentResult, register, login, forgotPassword, saveTopics, getTopics, getUser } = require("../Controller/authController");
+const { getRiskAssessmentResult, register, login, forgotPassword, saveTopics, getTopics, getUser, getUserDetailsById } = require("../Controller/authController");
 const router = express.Router();
 
 router.post("/register", register);
@@ -8,5 +8,6 @@ router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
 router.get("/:userId", getUser);
 router.get("/risk-assessment/:userId", getRiskAssessmentResult);
+router.get("/details/:userId", getUserDetails)
 
 module.exports = router;
