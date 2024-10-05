40377e92e25c8bbe9abd7a4e9c93c5362ef721f0
Bill <billhuillca@gmail.com>
2024-10-05 04:17:48 -0400

    transition to react

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import Registration from './Registration';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      react cleanup
+      <Registration/>
     </div>
   );
 }
