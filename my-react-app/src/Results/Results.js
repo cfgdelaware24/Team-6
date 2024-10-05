40696776b925b1603080a8be7d4302d016f000be
devinamunjal <devinamunjal@gmail.com>
2024-10-05 06:44:22 -0400

    addinginfobuttonsonbothresultpagesa

--- my-react-app/src/Results/Results.js
+++ my-react-app/src/Results/Results.js
@@ -1,9 +1,10 @@
-// Results.js
-
 import React from 'react';
-import './results.css'; // Import your CSS file
+import './results.css'; 
+import { useNavigate } from 'react-router-dom';
 
 function Results() {
+    const navigate = useNavigate();
+
     return (
         <div className="container">
             <div className="results-box">
@@ -13,8 +14,12 @@ function Results() {
                 </p>
                 <img src="image1.jpeg" alt="Heart in the Game" className="logo" />
                 <p className="suggestion">We suggest you take an <strong>EKG</strong> test based on your results</p>
-                <p className="suggestion">We suggest you volunteer or take more action</p>
-                <button className="volunteer-button">Click here to volunteer and help others get tested</button>
+                <p className="suggestion">We suggest you take more action</p>
+                
+                {}
+                <button className="info-button" onClick={() => navigate('/events-dashboard')}>
+                    Personal Profile
+                </button>
             </div>
         </div>
     );
